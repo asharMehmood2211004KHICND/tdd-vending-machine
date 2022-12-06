28fbdb3f5ddd628b83ab8fbe55299dbe24f43032
asharMehmood2211004KHICND <ashar.mehmood@xloopdigital.com>
2022-12-06 17:23:56 +0500

    test 2 passed

--- src/Machine.js
+++ src/Machine.js
@@ -12,5 +12,29 @@ module.exports = class Machine {
         
     }
 
+    deposit(amount_entered){
+        
+        let arr = [10,20,50,100,500];
+
+        // for(let i=0;i<=arr.length;i++){
+        //     if(amount_entered == arr[i]){
+        //         console.log(arr[i]);
+        //        return `You have deposited Rs ${arr[i]}`;
+        //     }else{
+        //         return 'invalid amount';
+        //     }
+        // }
+
+        if(arr.includes(amount_entered)){
+            // return `You have deposited Rs ${amount_entered}`;
+            return `You have deposited Rs ${amount_entered}`;
+        }else{
+            return 'invalid amount'
+        }
+    }
     
 };
+
+
+// const machine = new Machine();
+// machine.deposit(33);
---
+++ src/check.js
@@ -0,0 +1,6 @@
+const Machine = require('../src/Machine');
+
+const machine = new Machine();
+
+
+console.log(machine.deposit(20));
